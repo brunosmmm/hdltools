b37912e472f91b45642bcaf28e8798bd375fedfa
brunosmmm <brunosmmm@gmail.com>
2017-05-18 23:26:42 -0400

    added more stuff

--- assets/tests/sample.mm
+++ assets/tests/sample.mm
@@ -1,5 +1,6 @@
 //static declarations
 #register_size 32;
+#addr_mode byte;
 
 //registers
 register CONTROL;
@@ -7,3 +8,10 @@ register STATUS;
 
 //register fields
 field CONTROL.IRQEN position=0 access=RW description="Enable Interrupts";
+field CONTROL.STOP_ON_ERR position=1 access=RW description="Stop IP on video errors";
+field STATUS.IRQCLR position=7 access=RW description="Interrupt flag; write 1 to clear";
+
+//outputs from register bits
+output IRQ_EN source=CONTROL.IRQEN;
+output STOP_ON_ERROR source=CONTROL.STOP_ON_ERROR;
+output IRQ_ACK source=STATUS.IRQCLR;
