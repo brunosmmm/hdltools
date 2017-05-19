7dc8e4e4dcf49772ce61fd7b9241d5631bc4eeab
brunosmmm <brunosmmm@gmail.com>
2017-05-19 13:50:53 -0400

    more description

--- assets/tests/sample.mm
+++ assets/tests/sample.mm
@@ -10,8 +10,13 @@ register STATUS;
 field CONTROL.IRQEN position=0 access=RW description="Enable Interrupts";
 field CONTROL.STOP_ON_ERR position=1 access=RW description="Stop IP on video errors";
 field STATUS.IRQCLR position=7 access=RW description="Interrupt flag; write 1 to clear";
+field STATUS.VIDEOERR position=0 access=R description="Video error flag";
 
 //outputs from register bits
 output IRQ_EN source=CONTROL.IRQEN;
 output STOP_ON_ERROR source=CONTROL.STOP_ON_ERROR;
 output IRQ_ACK source=STATUS.IRQCLR;
+
+//input flags from logic
+input IRQ_FLAG dest=STATUS.IRQCLR;
+input VIDEO_ERROR dest=STATUS.VIDEOERR;
