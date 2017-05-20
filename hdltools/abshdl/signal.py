a7600cf99dbd139e3699f2ee86032eb7805b5450
brunosmmm <brunosmmm@gmail.com>
2017-05-20 15:33:26 -0400

    forgot signal implementation

---
+++ hdltools/abshdl/signal.py
@@ -0,0 +1,45 @@
+"""HDL Signals."""
+
+from . import HDLObject, HDLVectorDescriptor
+
+# TODO allow multiple dimensions
+
+
+class HDLSignal(HDLObject):
+    """HDL Signal."""
+
+    _types = ['comb', 'reg']
+
+    def __init__(self, sig_type, sig_name, size=1):
+        """Initialize."""
+        if sig_type not in self._types:
+            raise ValueError('invalid signal type: "{}"'.format(sig_type))
+
+        self.sig_type = sig_type
+        self.name = sig_name
+
+        if isinstance(size, int):
+            # default is [size-1:0] / (size-1 downto 0)
+            if (size < 0):
+                raise ValueError('only positive size allowed')
+            self.vector = HDLVectorDescriptor(size-1, 0)
+        elif isinstance(size, (tuple, list)):
+            if len(size) != 2:
+                raise ValueError('invalid vector '
+                                 'dimensions: "{}"'.format(size))
+            self.vector = HDLVectorDescriptor(*size)
+        elif isinstance(size, HDLVectorDescriptor):
+            self.vector = size
+        else:
+            raise TypeError('size can only be of types: int, list or'
+                            ' HDLVectorDescriptor')
+
+    def __repr__(self, eval_scope=None):
+        """Get readable representation."""
+        return '{} {}{}'.format(self.sig_type.upper(),
+                                self.name,
+                                self.vector.dumps(eval_scope))
+
+    def dumps(self, eval_scope=None):
+        """Alias for __repr__."""
+        return self.__repr__(eval_scope)
