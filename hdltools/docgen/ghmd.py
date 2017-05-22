6f9412ba2e35cd82d8f0fce70c504e9416626e1d
brunosmmm <brunosmmm@gmail.com>
2017-05-22 14:57:09 -0400

    github flavored markdown

---
+++ hdltools/docgen/ghmd.py
@@ -0,0 +1,44 @@
+"""GitHub flavored markdown."""
+
+from .markdown import MarkDownString, MarkDownDocument
+
+
+class GHMarkDownDocument(MarkDownDocument):
+    """GitHub flavored markdown document."""
+
+    pass
+
+
+class GHMarkDownTable(MarkDownString):
+    """Table."""
+
+    def __init__(self, headers):
+        """Initialize."""
+        self.lines = []
+        if not isinstance(headers, (tuple, list)):
+            raise TypeError('headers can only be tuple or list')
+        self.headers = headers
+
+    def add_line(self, *args):
+        """Add line."""
+        if len(args) != len(self.headers):
+            raise IndexError('incorrect number of'
+                             ' elements! need {}'.format(len(self.headers)))
+        self.lines.append(args)
+
+    def dumps(self):
+        """Dump Table."""
+        ret_str = ' | '.join(self.headers) + '\n'
+        hyphens = ['-'*len(x) for x in self.headers]
+        ret_str += ' | '.join(hyphens) + '\n'
+
+        for line in self.lines:
+            ret_str += ' | '.join(line) + '\n'
+
+        return ret_str
+
+
+class GHMarkDownTaskList(MarkDownString):
+    """Task List."""
+
+    pass
