12833f70e4368a999e39efa3bd005875976df3b4
brunosmmm <brunosmmm@gmail.com>
2017-05-22 15:31:43 -0400

    a simple documentation builder for mmap description

---
+++ tools/mmap_docgen.py
@@ -0,0 +1,61 @@
+#!/bin/python
+
+"""Build documentation from memory mapped descriptions."""
+
+import argparse
+import os
+from hdltools.abshdl.mmap import MemoryMappedInterface
+from hdltools.docgen.ghmd import (GHMarkDownTable, GHMarkDownDocument)
+from hdltools.docgen.markdown import (MarkDownHeader)
+
+
+if __name__ == "__main__":
+
+    # argument parser
+    arg_parser = argparse.ArgumentParser(description='Generate documentation'
+                                         ' from memory mapped interface'
+                                         ' description')
+
+    arg_parser.add_argument('model', help='Model file')
+    arg_parser.add_argument('--output', help='Output file', action='store')
+
+    args = arg_parser.parse_args()
+
+    mmap = MemoryMappedInterface()
+    mmap.parse_file(args.model)
+
+    doc = GHMarkDownDocument()
+
+    # build documentation
+    doc.append(MarkDownHeader('Memory Mapped Registers'), newline=True)
+    reg_table = GHMarkDownTable(['Address', 'Name', 'Description'])
+
+    for name, reg in mmap.registers.items():
+        if 'description' in reg.properties:
+            descr = reg.properties['description']
+        else:
+            descr = '---'
+        reg_table.add_line(reg.addr, name, descr)
+
+    doc.append(reg_table)
+
+    for name, reg in mmap.registers.items():
+        doc.append(MarkDownHeader('{} register details'.format(name),
+                                  level=2), newline=True)
+
+        reg_fields = GHMarkDownTable(['Bits', 'Name', 'Access', 'Description'])
+        for field in reg.fields:
+            if 'description' in field.properties:
+                descr = field.properties['description']
+            else:
+                descr = '---'
+            reg_fields.add_line(field.dumps_slice(), field.name,
+                                field.permissions, descr)
+        doc.append(reg_fields)
+
+    if args.output is not None:
+        with open(args.output, 'w') as f:
+            f.write(doc.dumps())
+
+    else:
+        print(doc)
