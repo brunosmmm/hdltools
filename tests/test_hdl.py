4c1937788b6f5a3c67718a2f3636677e78590a5e
brunosmmm <brunosmmm@gmail.com>
2017-05-22 08:02:44 -0400

    increase test coverage

--- tests/test_hdl.py
+++ tests/test_hdl.py
@@ -5,10 +5,13 @@ from hdltools.abshdl.module import (HDLModulePort,
                                     HDLModule,
                                     HDLModuleParameter)
 from hdltools.abshdl.expr import HDLExpression
+from hdltools.abshdl.signal import HDLSignal, HDLSignalSlice
+from hdltools.abshdl.const import HDLIntegerConstant
 import os
 import ast
 
-def test_hdl_primitives():
+# test HDL primitives
+def test_vector_descriptor():
 
     # basic testing
     vec = HDLVectorDescriptor(0, 0)
@@ -32,11 +35,19 @@ def test_hdl_primitives():
         pass
 
     try:
+        vec = HDLVectorDescriptor(left_size=1, right_size='1')
+        raise Exception
+    except TypeError:
+        pass
+
+    try:
         vec = HDLVectorDescriptor(7, stored_value='a')
         raise Exception
     except TypeError:
         pass
 
+    vec = HDLVectorDescriptor(8, stored_value=256)
+    left, right = vec.evaluate()
     fit = HDLVectorDescriptor.value_fits_width(8, 256)
     if fit is True:
         raise Exception
@@ -50,7 +61,8 @@ def test_hdl_primitives():
     except ValueError:
         pass
 
-    # ports
+def test_module_port():
+
     port = HDLModulePort('in', 'myport', 3)
     port = HDLModulePort('out', 'myport', (2, 0))
     port = HDLModulePort('inout', 'myport', HDLVectorDescriptor(2, 0))
@@ -81,11 +93,13 @@ def test_hdl_primitives():
     except TypeError:
         pass
 
-    # HDL Parameter
+def test_hdl_parameter():
+
     param = HDLModuleParameter('myparam', 'integer', param_default=0)
     print(param.dumps())
 
-    # HDL MODULE
+def test_hdl_module():
+
     mod = HDLModule('my_module')
     mod = HDLModule('my_module', [HDLModulePort('in', 'myport', 8)])
     mod = HDLModule('my_module', params=[HDLModuleParameter('myparam',
@@ -134,13 +148,70 @@ def test_hdl_primitives():
     except TypeError:
         pass
 
+def test_hdl_expression():
+
     expr_1 = 'PARAM-2'
     expr_2 = 'PARAM_X+1'
     hdl_expr_1 = HDLExpression(ast.parse(expr_1, mode='eval'))
     hdl_expr_2 = HDLExpression(ast.parse(expr_2, mode='eval'))
     sum = hdl_expr_1 + hdl_expr_2
     print(sum.dumps())
-    # raise
+
+    my_signal = HDLSignal('reg', 'signal_a', size=2)
+    _ = HDLExpression(HDLIntegerConstant(1))
+    _ = HDLExpression(1)
+    _ = HDLExpression(my_signal)
+    _ = HDLExpression(HDLSignalSlice(my_signal, 1))
+    _ = HDLExpression(my_signal[1:0])
+    try:
+        _ = HDLExpression('invalid')
+        raise Exception
+    except TypeError:
+        pass
+
+def test_hdl_signal():
+
+    my_sig = HDLSignal('reg', 'signal_x', size=(7, 0))
+    print(my_sig.dumps())
+    my_sliced_sig = my_sig[3:1]
+    other_slice = my_sig[7]
+    yet_another = my_sig[2:]
+    another = my_sig[:2]
+    print(yet_another.dumps())
+    _ = HDLSignal('reg', 'sig', HDLVectorDescriptor(1, 0))
+
+    # exceptions
+    try:
+        _ = HDLSignal('unknown', 'sig', 1)
+        raise Exception
+    except ValueError:
+        pass
+
+    try:
+        _ = HDLSignal('reg', 'sig', -1)
+        raise Exception
+    except ValueError:
+        pass
+
+    try:
+        _ = HDLSignal('reg', 'sig', (1, 2, 3))
+        raise Exception
+    except ValueError:
+        pass
+
+    try:
+        _ = HDLSignal('reg', 'sig', 'invalid')
+        raise Exception
+    except TypeError:
+        pass
+
+    _ = HDLSignalSlice(my_sig, HDLVectorDescriptor(1, 0))
+
+    try:
+        _ = HDLSignalSlice(my_sig, 'invalid')
+        raise Exception
+    except TypeError:
+        pass
 
 def test_verilog_parser():
 
