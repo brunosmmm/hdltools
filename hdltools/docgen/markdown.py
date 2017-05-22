7abefca10a15032b57c8631afd9aa24f51b91e32
brunosmmm <brunosmmm@gmail.com>
2017-05-22 14:43:12 -0400

    added simple markdown text generator

---
+++ hdltools/docgen/markdown.py
@@ -0,0 +1,159 @@
+"""Generate markdown documentation."""
+
+
+class MarkDownObject:
+    """Abstract class for markdown-related objects."""
+
+    def dumps(self):
+        """Dump representation."""
+        pass
+
+    def __str__(self):
+        """Alias for dumps."""
+        return self.dumps()
+
+
+class MarkDownString(MarkDownObject):
+    """Generic markdown text."""
+
+    def __init__(self, text):
+        """Initialize."""
+        self.text = str(text)
+
+    def __add__(self, other):
+        """Add operator."""
+        return str(self) + other
+
+    def __radd__(self, other):
+        """Reverse add operator."""
+        return other + str(self)
+
+
+class MarkDownItalic(MarkDownString):
+    """Italic text."""
+
+    def __init__(self, text):
+        """Initialize."""
+        super(MarkDownItalic, self).__init__(text)
+
+    def dumps(self):
+        """Get italic text."""
+        return '_{}_'.format(self.text)
+
+
+class MarkDownBold(MarkDownString):
+    """Bold text."""
+
+    def __init__(self, text):
+        """Initialize."""
+        super(MarkDownBold, self).__init__(text)
+
+    def dumps(self):
+        """Get bold text."""
+        return '**{}**'.format(self.text)
+
+
+class MarkDownHeader(MarkDownString):
+    """Markdown header."""
+
+    def __init__(self, text, level=1):
+        """Initialize."""
+        super(MarkDownHeader, self).__init__(text)
+        if level not in range(1, 7):
+            raise ValueError('heading level must be between 1 and 6')
+
+        self.level = level
+
+    def dumps(self):
+        """Dump string."""
+        ret_str = '#'*self.level + ' ' + self.text
+        return ret_str
+
+
+class MarkDownList(MarkDownString):
+    """Lists."""
+
+    def __init__(self, objects=None, ordered=False):
+        """Initialize."""
+        self.items = []
+        self.ordered = ordered
+        if objects is not None:
+            self.items.extend(objects)
+
+    def dumps(self):
+        """Dump list string."""
+        ret_str = ''
+        for count, item in enumerate(self.items):
+            if self.ordered is False:
+                ret_str += '* {}\n'.format(str(item))
+            else:
+                ret_str += '{}. {}\n'.format(count+1, str(item))
+
+        return ret_str
+
+
+class MarkDownLink(MarkDownString):
+    """URLs."""
+
+    def __init__(self, url, text=None):
+        """Initialize."""
+        super(MarkDownLink, self).__init__(text)
+        self.url = url
+
+    def dumps(self):
+        """Dump link."""
+        if self.text is None:
+            return str(self.url)
+        else:
+            return '[{}]({})'.format(str(self.text), self.url)
+
+
+class MarkDownQuote(MarkDownString):
+    """Block Quotes."""
+
+    def __init__(self, text, gobble=True):
+        """Initialize."""
+        super(MarkDownQuote, self).__init__(text)
+        self.gobble = gobble
+
+    def dumps(self):
+        """Dump quoted text."""
+        ret_str = ''
+        for line in self.text.split('\n'):
+            if self.gobble is True:
+                the_line = str(line).strip()
+            else:
+                the_line = str(line)
+            ret_str += '> {}\n'.format(the_line)
+
+        return ret_str
+
+
+class MarkDownCode(MarkDownString):
+    """Code blocks."""
+
+    def __init__(self, text):
+        """Initialize."""
+        super(MarkDownCode, self).__init__(text)
+
+    def dumps(self):
+        """Dump code."""
+        return '`{}`'.format(str(self.text))
+
+
+class MarkDownDocument(MarkDownObject):
+    """Markdown document."""
+
+    def __init__(self):
+        """Initialize."""
+        self.elements = []
+
+    def append(self, element, newline=False):
+        """Add element to document."""
+        self.elements.append(element)
+        if newline is True:
+            self.elements.append('\n')
+
+    def dumps(self):
+        """Dump document."""
+        return ''.join([str(x) for x in self.elements])
---
+++ tests/test_docgen.py
@@ -0,0 +1,67 @@
+from hdltools.docgen.markdown import (MarkDownList, MarkDownHeader,
+                                      MarkDownLink, MarkDownQuote,
+                                      MarkDownBold, MarkDownItalic,
+                                      MarkDownCode, MarkDownDocument)
+
+def test_mdlist():
+
+    bullets = MarkDownList(['some', 'items'])
+    ordered = MarkDownList(['ordered', 'items'], ordered=True)
+
+    print(bullets)
+    print(ordered)
+
+def test_mdheader():
+
+    heading_1 = MarkDownHeader('Header 1')
+    heading_2 = MarkDownHeader('Header 2', level=2)
+
+    print(heading_1)
+    print(heading_2)
+
+def test_mdlink():
+
+    link_1 = MarkDownLink('http://hello.world')
+    link_2 = MarkDownLink('http://hello.world', 'Hello World!')
+
+    print(link_1)
+    print(link_2)
+
+def test_mdquote():
+
+    quote_ungob = MarkDownQuote('Some Multiline\n   Text.', gobble=False)
+    quote_gob = MarkDownQuote('Some Multiline\n   Text.', gobble=True)
+
+    print(quote_ungob)
+    print(quote_gob)
+
+def test_mdtext():
+
+    bold = MarkDownBold('Bold Text')
+    italic = MarkDownItalic('Italic Text')
+
+    print(bold)
+    print(italic)
+
+def test_mdcode():
+
+    code = MarkDownCode('lambda x : x**2')
+
+    print(code)
+
+def test_mdstr():
+
+    text = MarkDownHeader('My header') + '\n' +\
+           MarkDownItalic('Some Italic Text') +\
+           '\nSome regular text'
+    print(text)
+
+def test_mddoc():
+
+    doc = MarkDownDocument()
+
+    doc.append(MarkDownHeader('My document'), newline=True)
+    doc.append('Some Text!!', newline=True)
+    doc.append(MarkDownList(['An element', 'Another Element']))
+
+    print(doc)
