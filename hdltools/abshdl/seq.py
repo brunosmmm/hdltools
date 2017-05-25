0cf0e1efeb3e0c4f42666ca87f3eaa7b214784b6
brunosmmm <brunosmmm@gmail.com>
2017-05-25 12:30:11 -0400

    added sequential block

---
+++ hdltools/abshdl/seq.py
@@ -0,0 +1,39 @@
+"""Sequential block."""
+
+from .stmt import HDLStatement
+from .scope import HDLScope
+from .sens import HDLSensitivityList
+
+
+class HDLSequentialBlock(HDLStatement):
+    """Sequential block."""
+
+    def __init__(self, sensitivity_list):
+        """Initialize."""
+        super(HDLSequentialBlock, self).__init__(stmt_type='seq')
+        self.scope = HDLScope(scope_type='seq')
+
+        # parse sensitivity list?
+        if not isinstance(sensitivity_list, HDLSensitivityList):
+            raise TypeError('only HDLSensitivityList allowed')
+
+        self.sens_list = sensitivity_list
+
+    def add(self, *items):
+        """Add to scope."""
+        self.scope.add(*items)
+
+    def dumps(self):
+        """Get representation."""
+        ret_str = 'SEQ({}) BEGIN\n'.format(self.sens_list.dumps())
+        ret_str += self.scope.dumps()
+        ret_str += '\nEND\n'
+
+        return ret_str
+
+    def is_legal(self):
+        """Check legality."""
+        if len(self.scope) == 0 or len(self.sens_list) == 0:
+            return False
+
+        return True
