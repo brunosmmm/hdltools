bad083c6f09b7511f1c7337eec0e27f11edc55e3
brunosmmm <brunosmmm@gmail.com>
2017-05-26 09:07:44 -0400

    add statement tag attribute

--- hdltools/abshdl/stmt.py
+++ hdltools/abshdl/stmt.py
@@ -8,12 +8,21 @@ class HDLStatement(HDLObject):
 
     _stmt_types = ['seq', 'par']
 
-    def __init__(self, stmt_type):
+    def __init__(self, stmt_type, tag=None):
         """Initialize."""
         if stmt_type not in self._stmt_types:
             raise KeyError('invalid statement type')
 
         self.stmt_type = stmt_type
+        self.tag = tag
+
+    def set_tag(self, tag):
+        """Set a tag."""
+        self.tag = tag
+
+    def get_tag(self):
+        """Get tag."""
+        return self.tag
 
     def is_legal(self):
         """Validate statement."""
