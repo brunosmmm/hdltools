c94684a097fa2ae4c3d9d246b4d8548e8bfa6113
brunosmmm <brunosmmm@gmail.com>
2017-05-26 10:33:18 -0400

    allow assignment to output ports

--- hdltools/abshdl/assign.py
+++ hdltools/abshdl/assign.py
@@ -5,6 +5,7 @@ from .expr import HDLExpression
 from .signal import HDLSignal, HDLSignalSlice
 from .concat import HDLConcatenation
 from .stmt import HDLStatement
+from .module import HDLModulePort
 
 
 class HDLAssignment(HDLStatement):
@@ -12,6 +13,12 @@ class HDLAssignment(HDLStatement):
 
     def __init__(self, signal, value, assign_type='block'):
         """Initialize."""
+        if isinstance(signal, HDLModulePort):
+            if signal.direction in ('out', 'inout'):
+                signal = signal.signal
+            else:
+                raise ValueError('cannot assign to input port')
+
         if not isinstance(signal, (HDLSignal, HDLSignalSlice)):
             raise TypeError('only HDLSignal, HDLSignalSlice can be assigned')
 
