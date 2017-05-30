e55d71cc6de59be4109324afc984f7abe1da01fc
brunosmmm <brunosmmm@gmail.com>
2017-05-30 18:11:23 -0400

    provide similar block building functionality with decorators

--- hdltools/hdllib/patterns.py
+++ hdltools/hdllib/patterns.py
@@ -3,6 +3,7 @@
 from ..abshdl.seq import HDLSequentialBlock
 from ..abshdl.ifelse import HDLIfElse
 from ..abshdl.sens import HDLSensitivityDescriptor, HDLSensitivityList
+from functools import wraps
 
 
 def get_sequential_block(sens_list, *stmts, **kwargs):
@@ -57,3 +58,88 @@ def get_reset_if_else(rst_signal, rst_lvl, rst_stmts, stmts=None, **kwargs):
                        else_scope=stmts)
 
     return ifelse
+
+
+class SequentialBlock:
+    """Sequential block decorator."""
+
+    def __init__(self, *args):
+        """Initialize."""
+        self.signals = args
+
+    def __call__(self, fn):
+        """Decorate."""
+        @wraps(fn)
+        def wrapper_SequentialBlock(*args):
+            seq = self.get(*self.signals)
+            ret = fn(seq, *args)
+            return ret
+        return wrapper_SequentialBlock
+
+    @staticmethod
+    def get(*args):
+        """Get sequential block."""
+        sens_descrs = []
+        for arg in args:
+            if isinstance(arg, (tuple, list)):
+                sens_descrs.append(HDLSensitivityDescriptor(*arg))
+            else:
+                sens_descrs.append(HDLSensitivityDescriptor('rise', arg))
+            sens_list = HDLSensitivityList(*sens_descrs)
+        seq = HDLSequentialBlock(sens_list)
+        return seq
+
+
+class ClockedBlock(SequentialBlock):
+    """Clocked sequential block."""
+
+    def __init__(self, clk, edge='rise'):
+        """Initialize."""
+        self.clk = clk
+        self.edge = edge
+
+    def __call__(self, fn):
+        """Decorate."""
+        @wraps(fn)
+        def wrapper_ClockedBlock(*args):
+            seq = self.get(self.clk, self.edge)
+            ret = fn(seq, *args)
+            return ret
+        return wrapper_ClockedBlock
+
+    @staticmethod
+    def get(clk, edge='rise'):
+        """Get Clocked block."""
+        sens_list = HDLSensitivityList(HDLSensitivityDescriptor(edge,
+                                                                clk))
+        seq = HDLSequentialBlock(sens_list)
+        return seq
+
+
+class ClockedRstBlock(ClockedBlock):
+    """Clocked sequential block with reset."""
+
+    def __init__(self, clk, rst, clk_edge='rise', rst_lvl=1):
+        """Initialize."""
+        self.rst = rst
+        self.lvl = rst_lvl
+        self.clk = clk
+        self.edge = clk_edge
+
+    def __call__(self, fn):
+        """Decorate."""
+        @wraps(fn)
+        def wrapper_ClockedRstBlock(*args, **kwargs):
+            # do stuff
+            seq = self.get(self.clk, self.rst, self.edge, self.lvl)
+            ret = fn(seq, *args, **kwargs)
+            return ret
+        return wrapper_ClockedRstBlock
+
+    @staticmethod
+    def get(clk, rst, edge='rise', lvl='1'):
+        """Get sequential block."""
+        seq = ClockedBlock.get(clk, edge)
+        rst_if = HDLIfElse(rst == lvl, tag='rst_if')
+        seq.add(rst_if)
+        return seq
