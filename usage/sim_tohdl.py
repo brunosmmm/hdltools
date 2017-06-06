048313146c8c8d6870404a8541cfd455f7f23242
brunosmmm <brunosmmm@gmail.com>
2017-06-05 20:27:14 -0400

    LFSR simulation model, that can be converted to HDL

--- usage/sim_tohdl.py
+++ usage/sim_tohdl.py
@@ -23,12 +23,42 @@ class Multiplexer(HDLSimulationObject):
         self.y = self.b if self.sel is True else self.a
 
 
+class LFSR(HDLSimulationObject):
+    """Linear feedback shift register."""
+
+    def __init__(self, identifier):
+        """Initialize."""
+        super().__init__(identifier)
+        self.clk = self.input('clk')
+        self.rst = self.input('rst')
+        self.en = self.input('en')
+        self.out = self.output('out', size=8)
+
+    def logic(self, **kwargs):
+        """Do internal logic."""
+        feedback = not (self.out[7] ^ self.out[3])
+
+        if self.rising_edge('clk'):
+            if self.rst is True:
+                self.out = 0
+            else:
+                if self.en is True:
+                    self.out = [self.out[6:0], feedback]
+                else:
+                    self.out = self.out
+
+
 if __name__ == "__main__":
 
     logic = Multiplexer('mux')
     sched = HDLSimulationObjectScheduler(logic)
-    gen = VerilogCodeGenerator()
+    gen = VerilogCodeGenerator(indent=True)
 
     # verilog code
     print('*Multiplexer Verilog Code*')
+    print(gen.dump_element(sched.schedule()))
+
+    lfsr = LFSR('lfsr')
+    sched = HDLSimulationObjectScheduler(lfsr)
+    print('*LFSR Verilog Code*')
     print(gen.dump_element(sched.schedule()))
