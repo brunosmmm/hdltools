27c6f685b70310448f8e00e1b2ec0a8149d58734
Bruno Morais <brunosmmm@gmail.com>
2019-04-09 09:43:28 -0400

    refactor more

--- tests/test_vloggen.py
+++ tests/test_vloggen.py
@@ -1,9 +1,8 @@
 """Test verilog codegen."""
 
 from hdltools.abshdl.ifelse import HDLIfElse
-from hdltools.abshdl.scope import HDLScope
 from hdltools.abshdl.expr import HDLExpression
-from hdltools.abshdl.signal import HDLSignal, HDLSignalSlice
+from hdltools.abshdl.signal import HDLSignal
 from hdltools.abshdl.assign import HDLAssignment
 from hdltools.abshdl.sens import HDLSensitivityList, HDLSensitivityDescriptor
 from hdltools.abshdl.seq import HDLSequentialBlock
@@ -13,12 +12,12 @@ from hdltools.verilog.codegen import VerilogCodeGenerator
 
 
 def test_ifelse():
-
+    """Test if-else."""
     # create an if-else block
     gen = VerilogCodeGenerator()
 
-    sig = HDLSignal(sig_type='comb', sig_name='test', size=1)
-    test_sig = HDLSignal(sig_type='reg', sig_name='counter', size=2)
+    sig = HDLSignal(sig_type="comb", sig_name="test", size=1)
+    test_sig = HDLSignal(sig_type="reg", sig_name="counter", size=2)
 
     assign_rhs = HDLExpression(test_sig) + 1
     assignment = HDLAssignment(signal=test_sig, value=assign_rhs)
@@ -32,21 +31,22 @@ def test_ifelse():
 
     print(gen.dump_element(ifelse))
 
-def test_always():
 
+def test_always():
+    """Test always block."""
     gen = VerilogCodeGenerator()
 
-    sig = HDLSignal(sig_type='reg', sig_name='clk', size=1)
-    sens = HDLSensitivityDescriptor(sens_type='rise', sig=sig)
+    sig = HDLSignal(sig_type="reg", sig_name="clk", size=1)
+    sens = HDLSensitivityDescriptor(sens_type="rise", sig=sig)
     sens_list = HDLSensitivityList()
     sens_list.add(sens)
 
-    test_sig = HDLSignal(sig_type='reg', sig_name='counter', size=2)
+    test_sig = HDLSignal(sig_type="reg", sig_name="counter", size=2)
     rst_assign = HDLAssignment(signal=test_sig, value=0)
     norm_expr = HDLExpression(test_sig) + 1
     norm_assign = HDLAssignment(signal=test_sig, value=norm_expr)
 
-    rst = HDLSignal(sig_type='reg', sig_name='rst', size=1)
+    rst = HDLSignal(sig_type="reg", sig_name="rst", size=1)
     ifelse = HDLIfElse(condition=rst)
     ifelse.add_to_if_scope(rst_assign)
     ifelse.add_to_else_scope(norm_assign)
@@ -56,17 +56,19 @@ def test_always():
 
     print(gen.dump_element(seq))
 
-def test_module():
 
+def test_module():
+    """Test module."""
     gen = VerilogCodeGenerator()
 
-    inp = HDLModulePort(direction='in', name='an_input', size=4)
-    out = HDLModulePort(direction='out', name='an_output', size=1)
-    prm = HDLModuleParameter(param_name='a_parameter',
-                             param_type='integer',
-                             param_default=HDLIntegerConstant(0))
+    inp = HDLModulePort(direction="in", name="an_input", size=4)
+    out = HDLModulePort(direction="out", name="an_output", size=1)
+    prm = HDLModuleParameter(
+        param_name="a_parameter",
+        param_type="integer",
+        param_default=HDLIntegerConstant(0),
+    )
 
-    mod = HDLModule(module_name='my_module', ports=[inp, out],
-                    params=[prm])
+    mod = HDLModule(module_name="my_module", ports=[inp, out], params=[prm])
 
     print(gen.dump_element(mod))
