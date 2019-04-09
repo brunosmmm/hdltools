1a937cb519c71c1b25a85f5e7a30288f423f40d9
Bruno Morais <brunosmmm@gmail.com>
2019-04-09 10:17:29 -0400

    use self to set state, refactor

--- usage/fsm.py
+++ usage/fsm.py
@@ -13,34 +13,41 @@ class TestFSM(FSM):
     def __state_zero(self, start):
         if start == 1:
             done_reg = 0
-            state = 'one'
+            self.state = "one"
 
     def __state_one(self, counter):
         counter = counter + 1
         if counter == 4:
-            state = 'two'
+            self.state = "two"
 
     def __state_two(self):
         done_reg = 1
-        state = 'zero'
+        self.state = "zero"
 
 
 if __name__ == "__main__":
 
     HDLBlock.add_custom_block(TestFSM)
 
-    @HDLModule('fsm', ports=[input_port('clk'),
-                             input_port('rst'),
-                             input_port('start'),
-                             output_port('done')])
-    def fsm_module(mod):
+    @HDLModule(
+        "fsm",
+        ports=[
+            input_port("clk"),
+            input_port("rst"),
+            input_port("start"),
+            output_port("done"),
+        ],
+    )
+    def fsm_module(mod, counter_size=8):
         """FSM Module."""
         # signals
-        mod.add([
-            HDLSignal('reg', 'state', size='defer'),
-            HDLSignal('reg', 'done_reg'),
-            HDLSignal('reg', 'counter', size=8)
-        ])
+        mod.add(
+            [
+                HDLSignal("reg", "state", size="defer"),
+                HDLSignal("reg", "done_reg"),
+                HDLSignal("reg", "counter", size=counter_size),
+            ]
+        )
 
         @HDLBlock(mod)
         @ParallelBlock()
@@ -48,7 +55,7 @@ if __name__ == "__main__":
             done = done_reg
             # sequential block generation
 
-            @TestFSM(clk, rst, state, initial='zero')
+            @TestFSM(clk, rst, state, initial="zero")
             def myfsm():
                 pass
 
@@ -56,7 +63,8 @@ if __name__ == "__main__":
         mod.extend(*fsm_body())
 
     # generate module
-    fsm = fsm_module()
+    # FIXME doesnt work with keyword arguments
+    fsm = fsm_module(16)
 
     print(fsm.dumps())
 
