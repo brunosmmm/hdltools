685ef21a72fb8d915c74a67554fe8faf01422935
Bruno Morais <brunosmmm@gmail.com>
2020-01-15 08:22:09 -0500

    implemented a rising_edge function

--- hdltools/sim/util.py
+++ hdltools/sim/util.py
@@ -1,8 +1,27 @@
 """Utility functions."""
 
 from ..abshdl.concat import HDLConcatenation
+from ..hdllib.patterns import SequentialBlock
+
+# globals
+_block_count = 0
 
 
 def concat(value, *args):
     """Concatenate."""
     return HDLConcatenation(value, *args, direction="lr")
+
+
+def rising_edge(signal):
+    """Rising edge."""
+    global _block_count
+
+    @SequentialBlock()
+    def gen_block():
+        # bla bla
+        pass
+
+    gen_block.__name__ = f"gen_{_block_count}"
+    _block_count += 1
+
+    return gen_block
