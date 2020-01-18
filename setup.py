01bf2e812eb1104c1682bf21722ce62c9fa96ea1
Bruno Morais <brunosmmm@gmail.com>
2020-01-18 09:03:02 -0500

    update/refactor setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,10 @@ setup(
     name="hdltools",
     version="0.1",
     packages=find_packages(),
-    package_dir={'': '.'},
-
+    package_dir={"": "."},
     install_requires=[],
-
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="HDL Tools",
-
-    scripts=['tools/mmap_docgen', 'tools/axi_slave_builder'],
-    )
+    scripts=["tools/mmap_docgen", "tools/axi_slave_builder", "tools/inputgen"],
+)
