dc874945533d2de022a3b751340a9313d78f45d4
Bruno Morais <brunosmmm@gmail.com>
2020-01-18 09:11:01 -0500

    fix dependency

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     package_dir={"": "."},
-    install_requires=[],
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="HDL Tools",
-    dependency_links=[
-        "http://github.com/brunosmmm/scoff/tarball/master#egg=scoff-0.1",
-        "http://github.com/brunosmmm/dictator/tarball/master#egg=dictator-0.1",
+    install_requires=[
+        "scoff @ git+https://github.com/brunosmmm/scoff@master#egg=scoff",
+        "dictator @ git+https://github.com/brunosmmm/dictator@master#egg=dictator",
     ],
     scripts=["tools/mmap_docgen", "tools/axi_slave_builder", "tools/inputgen"],
 )
