4768711d75d8c29b0f3ce8f1c43770a439131860
Bruno Morais <brunosmmm@gmail.com>
2020-01-18 09:06:48 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -11,5 +11,9 @@ setup(
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="HDL Tools",
+    dependency_links=[
+        "http://github.com/brunosmmm/scoff/tarball/master#egg=scoff-0.1",
+        "http://github.com/brunosmmm/dictator/tarball/master#egg=dictator-0.1",
+    ],
     scripts=["tools/mmap_docgen", "tools/axi_slave_builder", "tools/inputgen"],
 )
