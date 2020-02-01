09587113eca49cd50da71b42024b2f1dff0fc0c9
Bruno Morais <brunosmmm@gmail.com>
2020-01-31 22:42:58 -0500

    start work on pattern matching, building block for trigger-like mechanism

---
+++ hdltools/patterns/__init__.py
@@ -0,0 +1,47 @@
+"""Signal pattern matching."""
+
+import re
+
+
+class PatternError(Exception):
+    """Pattern error."""
+
+
+class Pattern:
+    """Signal pattern representation."""
+
+    PATTERN_REGEX = re.compile(r"[01xX]+")
+
+    def __init__(self, pattern: str):
+        """Initialize."""
+        if not isinstance(pattern, str):
+            raise TypeError("pattern must be a string")
+        m = self.PATTERN_REGEX.match(pattern)
+        if m is None:
+            raise PatternError("pattern is invalid")
+
+        self._pattern = pattern
+
+    @property
+    def pattern(self):
+        """Get pattern."""
+        return self._pattern
+
+    def __len__(self):
+        """Get length."""
+        return len(self._pattern)
+
+    def match(self, value):
+        """Match against value."""
+        if not isinstance(value, str):
+            raise TypeError("value must be string")
+        if len(value) != len(self._pattern):
+            raise ValueError("cannot compare, different lengths")
+        for value_bit, expected_bit in zip(value, self._pattern):
+            if expected_bit in ("x", "X"):
+                # don't care
+                continue
+            if expected_bit != value_bit:
+                return False
+
+        return True
---
+++ hdltools/patterns/matcher.py
@@ -0,0 +1,5 @@
+"""Pattern matcher."""
+
+
+class PatternMatcher:
+    """Pattern matcher state machine."""
