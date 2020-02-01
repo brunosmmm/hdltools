413610316559c9c60201752aad6a6c716333accb
Bruno Morais <brunosmmm@gmail.com>
2020-02-01 09:40:14 -0500

    add vcd test

---
+++ tests/test_vcd.py
@@ -0,0 +1,12 @@
+"""Test VCD extensions."""
+
+from hdltools.vcd.parser import BaseVCDParser
+
+
+def test_parser():
+    """Test VCD parser."""
+    with open("tests/assets/example.vcd", "r") as vcdfile:
+        vcd_data = vcdfile.read()
+
+    vparser = BaseVCDParser()
+    vparser.parse(vcd_data)
