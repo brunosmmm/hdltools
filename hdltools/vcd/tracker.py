1048d1aef04ef0405dc9c8d72f1cad1542a44806
Bruno Morais <brunosmmm@gmail.com>
2020-02-03 07:55:14 -0500

    WIP value tracker

--- hdltools/vcd/tracker.py
+++ hdltools/vcd/tracker.py
@@ -11,6 +11,45 @@ from hdltools.vcd import VCDVariable
 from hdltools.patterns import Pattern
 
 
+class ScopeMap:
+    """Scope map."""
+
+    def __init__(self):
+        """Initialize."""
+        self._map = {}
+
+    def __getitem__(self, index):
+        """Get item."""
+        if not isinstance(index, tuple):
+            raise TypeError("index must be a tuple")
+
+        current_map = self._map
+        for element in index:
+            current_map = current_map[element]
+
+        return current_map
+
+    def add_hierarchy(self, index, hier_name):
+        """Add hierarchy."""
+        outer = self[index]
+        if hier_name in outer:
+            raise ValueError("scope already exists.")
+        outer[hier_name] = {}
+
+    def dump(self):
+        """Print out."""
+        ScopeMap._dump(self._map)
+
+    @staticmethod
+    def _dump(hier, indent=0):
+        """Print out."""
+
+        current_indent = "  " * indent
+        for name, _hier in hier.items():
+            print(current_indent + name)
+            ScopeMap._dump(_hier, indent + 1)
+
+
 # TODO: multi value tracker
 class VCDValueTracker(BaseVCDParser):
     """VCD Value tracker.
@@ -20,12 +59,23 @@ class VCDValueTracker(BaseVCDParser):
 
     def __init__(self, track: Pattern):
         """Initialize."""
+        super().__init__()
         if not isinstance(track, Pattern):
             raise TypeError("track must be a Pattern object")
         self._track_value = track
         self._track_history = []
         self._scope_stack = deque()
+        self._scope_map = ScopeMap()
         self._vars = {}
+
+    def _enter_scope(self, name):
+        """Enter scope."""
+        self._scope_map.add_hierarchy(self.current_scope, name)
+        self._scope_stack.append(name)
+
+    def _exit_scope(self):
+        """Exit scope."""
+        self._scope_stack.popleft()
 
     def header_statement_handler(self, stmt, fields):
         """Handle header statements.
@@ -34,15 +84,15 @@ class VCDValueTracker(BaseVCDParser):
         """
         if stmt == SCOPE_PARSER:
             # enter scope
-            self._scope_stack.append(fields["sname"])
+            self._enter_scope(fields["sname"])
             return
 
         if stmt == UPSCOPE_PARSER:
-            self._scope_stack.popleft()
+            self._exit_scope()
             return
 
         if stmt == VAR_PARSER:
-            var = VCDVariable.from_tokens(scope=self.curent_scope, **fields)
+            var = VCDVariable.from_tokens(scope=self.current_scope, **fields)
             self._vars[fields["id"]] = var
 
     def initial_value_handler(self, stmt, fields):
@@ -56,7 +106,7 @@ class VCDValueTracker(BaseVCDParser):
         """Handle value change."""
         if self._track_value.match(fields["value"]):
             var = self._vars[fields["var"]]
-            self._add_to_history(var.scope, var.name)
+            self._add_to_history(var.scope, var.name, self.current_time)
 
     @property
     def current_scope_depth(self):
@@ -68,6 +118,11 @@ class VCDValueTracker(BaseVCDParser):
         """Get current scope."""
         return tuple(self._scope_stack)
 
-    def _add_to_history(self, scope, signal):
+    @property
+    def scope_hier(self):
+        """Get scope hierarchy."""
+        return self._scope_map
+
+    def _add_to_history(self, scope, signal, time):
         """Add to history."""
-        self._track_history.append((scope, signal))
+        self._track_history.append((scope, signal, time))
