db6070ec83941b5a9b6691f4e8049a182df07f81
Bruno Morais <brunosmmm@gmail.com>
2020-02-03 14:31:31 -0500

    allow bytearrays for patterns

--- hdltools/patterns/__init__.py
+++ hdltools/patterns/__init__.py
@@ -1,6 +1,7 @@
 """Signal pattern matching."""
 
 import re
+from typing import Union
 
 
 class PatternError(Exception):
@@ -11,12 +12,16 @@ class Pattern:
     """Signal pattern representation."""
 
     PATTERN_REGEX = re.compile(r"[01xX]+")
+    PATTERN_REGEX_BYTES = re.compile(b"[01xX]+")
 
-    def __init__(self, pattern: str):
+    def __init__(self, pattern: Union[str, bytes]):
         """Initialize."""
-        if not isinstance(pattern, str):
-            raise TypeError("pattern must be a string")
-        m = self.PATTERN_REGEX.match(pattern)
+        if not isinstance(pattern, (str, bytes)):
+            raise TypeError("pattern must be a string or bytes")
+        if isinstance(pattern, str):
+            m = self.PATTERN_REGEX.match(pattern)
+        else:
+            m = self.PATTERN_REGEX_BYTES.match(pattern)
         if m is None:
             raise PatternError("pattern is invalid")
 
@@ -31,10 +36,13 @@ class Pattern:
         """Get length."""
         return len(self._pattern)
 
-    def match(self, value: str) -> bool:
+    def match(self, value: Union[str, bytes]) -> bool:
         """Match against value."""
-        if not isinstance(value, str):
-            raise TypeError("value must be string")
+        if not isinstance(value, (str, bytes)):
+            raise TypeError("value must be string or bytes")
+
+        if type(value) != type(self._pattern):
+            raise TypeError("incompatible types for value and pattern")
 
         pattern = self._pattern
         if len(value) < len(self._pattern):
