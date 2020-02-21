963f02937a78df44314a08f614843be6aa19242c
Bruno Morais <brunosmmm@gmail.com>
2020-02-21 15:42:57 -0500

    add tests for VCDScope class

--- tests/test_vcd.py
+++ tests/test_vcd.py
@@ -1,6 +1,8 @@
 """Test VCD extensions."""
 
+import pytest
 from hdltools.vcd.parser import BaseVCDParser
+from hdltools.vcd import VCDScope
 
 
 def test_parser():
@@ -10,6 +12,37 @@ def test_parser():
 
     vparser = BaseVCDParser()
     vparser.parse(vcd_data)
+
+def test_vcd_scope():
+    """Test VCDScope."""
+    scope = VCDScope("some", "scopes", "inside")
+    similar_scope = VCDScope("some", "scopes", "inside")
+    other_scope = VCDScope("some", "other", "scopes")
+    contained_scope = VCDScope("some", "scopes", "inside", "scopes")
+
+    assert (scope != other_scope)
+    assert (scope == similar_scope)
+    assert(repr(scope) == "some::scopes::inside")
+    assert(scope.contains(contained_scope))
+    assert(not scope.contains(other_scope))
+    assert(len(scope) == 3)
+    assert(scope[0] == "some")
+
+
+    with pytest.raises(TypeError):
+        scope == None
+
+def test_vcd_scope_fromstr():
+    """Test building VCDScope from string."""
+
+    scope, inclusive = VCDScope.from_str("VCD::Scope::example")
+    assert(inclusive is False)
+    scope, inclusive = VCDScope.from_str("VCD::Scope::example::")
+    assert(inclusive is True)
+
+    with pytest.raises(TypeError):
+        scope = VCDScope.from_str(123)
+
 
 
 if __name__ == "__main__":
