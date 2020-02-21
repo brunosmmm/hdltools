6ae225cd44e91439bdf146339bab7c5b347b4b28
Bruno Morais <brunosmmm@gmail.com>
2020-02-21 17:28:31 -0500

    VCDTriggerEvent implementation

--- hdltools/vcd/trigger.py
+++ hdltools/vcd/trigger.py
@@ -1,11 +1,14 @@
 """VCD value trigger."""
+
 import re
+from typing import Optional
 from hdltools.vcd import VCDObject, VCDScope
 from hdltools.patterns import Pattern
 
 
 class VCDTriggerError(Exception):
     """Trigger error."""
+
 
 
 class VCDTriggerDescriptor(VCDObject):
@@ -63,3 +66,41 @@ class VCDTriggerDescriptor(VCDObject):
     def __repr__(self):
         """Get representation."""
         return "{{{}::{}=={}}}".format(str(self.scope), self.name, self.value)
+
+
+class VCDTriggerEvent(VCDObject):
+    """VCD trigger event."""
+
+    EVENT_TYPES = ("condition", "trigger")
+
+    def __init__(self, evt_type: str, time: int,
+                 evt: Optional[VCDTriggerDescriptor] = None):
+        """Initialize."""
+        if not isinstance(time, int):
+            raise TypeError("time must be an integer")
+        self._time = time
+        if evt_type not in self.EVENT_TYPES:
+            raise ValueError("invalid event type")
+        self._type = evt_type
+
+        if evt is None:
+            self._evt = None
+        elif not isinstance(evt, VCDTriggerDescriptor):
+            raise TypeError("evt must be a VCDTriggerDescriptor object")
+        else:
+            self._evt = evt
+
+    @property
+    def time(self):
+        """Get occurrence time."""
+        return self._time
+
+    @property
+    def evt_type(self):
+        """Get event type."""
+        return self._type
+
+    @property
+    def evt(self):
+        """Get trigger descriptor."""
+        return self._evt
