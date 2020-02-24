104f58854ebf5dcea9866e9122be512034b502e2
Bruno Morais <brunosmmm@gmail.com>
2020-02-24 13:43:42 -0500

    Implement crude grammar to parse gnu objdump output

---
+++ hdltools/binutils/__init__.py
@@ -0,0 +1,23 @@
+"""Binary analysis utilities."""
+
+import pkg_resources
+from textx.metamodel import metamodel_from_file
+
+METAMODEL_FILE = pkg_resources.resource_filename(
+    "hdltools", "binutils/objdump.tx"
+)
+OBJDUMP_METAMODEL = metamodel_from_file(METAMODEL_FILE)
+
+
+def parse_objdump(text):
+    """Parse vecgen file."""
+    model = OBJDUMP_METAMODEL.model_from_str(text)
+    return model
+
+
+def parse_objdump_file(path):
+    """Parse from file."""
+    with open(path, "r") as vecf:
+        contents = vecf.read()
+
+    return parse_objdump(contents)
