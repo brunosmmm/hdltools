b6afc2de343b345ac3c53fd7b845e57eb31a249d
Bruno <brunosmmm@gmail.com>
2022-02-03 15:10:50 -0500

    Use alias

--- hdltools/hdllib/axi/interfaces.py
+++ hdltools/hdllib/axi/interfaces.py
@@ -6,24 +6,29 @@ from hdltools.abshdl.interface import HDLModuleInterface
 class AXI4LiteSlaveIf(HDLModuleInterface):
     """AXI4 Lite slave interface."""
 
+    _ALIASES = {
+        "data_width": ("C_S_AXI_DATA_WIDTH"),
+        "addr_width": ("C_S_AXI_ADDR_WIDTH"),
+    }
+
     _PORTS = {
         "_aclk": {"dir": "input", "size": 1, "flips": False},
         "_aresetn": {"dir": "input", "size": 1, "flips": False},
         "_awvalid": {"dir": "input", "size": 1},
-        "_awaddr": {"dir": "input", "size": "C_S_AXI_ADDR_WIDTH"},
+        "_awaddr": {"dir": "input", "size": "addr_width"},
         "_awready": {"dir": "output", "size": 1},
         "_wvalid": {"dir": "input", "size": 1},
-        "_wdata": {"dir": "input", "size": "C_S_AXI_DATA_WIDTH"},
-        "_wstrb": {"dir": "input", "size": "C_S_AXI_DATA_WIDTH / 8"},
+        "_wdata": {"dir": "input", "size": "data_width"},
+        "_wstrb": {"dir": "input", "size": "data_width / 8"},
         "_wready": {"dir": "output", "size": 1},
         "_bvalid": {"dir": "output", "size": 1},
         "_bresp": {"dir": "output", "size": 2},
         "_bready": {"dir": "input", "size": 1},
         "_arvalid": {"dir": "input", "size": 1},
-        "_araddr": {"dir": "input", "size": "C_S_AXI_ADDR_WIDTH"},
+        "_araddr": {"dir": "input", "size": "addr_width"},
         "_arready": {"dir": "output", "size": 1},
         "_rvalid": {"dir": "output", "size": 1},
-        "_rdata": {"dir": "output", "size": "C_S_AXI_DATA_WIDTH"},
+        "_rdata": {"dir": "output", "size": "data_width"},
         "_rresp": {"dir": "output", "size": 2},
         "_rready": {"dir": "input", "size": 1},
         "_awprot": {"dir": "input", "size": 3},
