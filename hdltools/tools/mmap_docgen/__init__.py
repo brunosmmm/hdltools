5337eaee94ab2b4236343afbe08d552f8b6ebdc9
Bruno Morais <brunosmmm@gmail.com>
2024-03-22 08:34:30 -0400

    Use param replacements in mmap_docgen

--- hdltools/tools/mmap_docgen/__init__.py
+++ hdltools/tools/mmap_docgen/__init__.py
@@ -3,11 +3,29 @@
 """Build documentation from memory mapped descriptions."""
 
 import argparse
+import logging
+from rich.logging import RichHandler
+from rich.console import Console
 
 from hdltools.mmap.builder import MMBuilder
 from hdltools.mmap import parse_mmap_file
 from hdltools.docgen.ghmd import GHMarkDownTable, GHMarkDownDocument
 from hdltools.docgen.markdown import MarkDownHeader
+from hdltools.tools.common.mmap import (
+    add_param_replace_args,
+    parse_param_replace_args,
+    MmapError,
+)
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler(console=Console(stderr=True))],
+)
+
+logger = logging.getLogger("hdltools.mmap_docgen")
 
 
 def main():
@@ -20,13 +38,20 @@ def main():
 
     arg_parser.add_argument("model", help="Model file")
     arg_parser.add_argument("--output", help="Output file", action="store")
+    add_param_replace_args(arg_parser)
 
     args = arg_parser.parse_args()
+
+    try:
+        param_replacements = parse_param_replace_args(args)
+    except MmapError as ex:
+        logger.error(str(ex))
+        exit(1)
 
     _, mmap = parse_mmap_file(args.model)
     text, mmap_model = parse_mmap_file(args.model)
     mmbuilder = MMBuilder(text)
-    mmap = mmbuilder.visit(mmap_model)
+    mmap = mmbuilder.visit(mmap_model, param_replace=param_replacements)
     doc = GHMarkDownDocument()
 
     # build documentation
